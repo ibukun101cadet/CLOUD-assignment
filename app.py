9e2341ede59e32041318ccca7581e22a24cc5066
ODUNTAN IBUKUNOLUWA 18CJ024139 <63315272+ibukun101cadet@users.noreply.github.com>
2022-01-13 11:03:13 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,12 @@
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+from flask import Flask, render_template
 
  
 app = Flask(__name__, template_folder = 'templates')
 
-app.config['MYSQL_HOST'] = 'myazure134.mysql.database.azure.com'
-app.config['MYSQL_USER'] = 'Ibukun_33@myazure134'
-app.config['MYSQL_PASSWORD'] = 'Forestgump_33'
-app.config['MYSQL_DB'] = 'myazure134'
-
-mysql = MySQL(app)
 
 @app.route('/')
 def index():
-        cur = mysql.connection.cursor()
-
-
-        show_database = """SELECT * FROM gender2"""
-        cur.execute(show_database)
-        data = cur.fetchall()
-        
-        
-        mysql.connection.commit()
-        cur.close()
-
-
-                
-
-        return render_template('index.html', value = data)
+        return render_template('index.html')
 
  
 if __name__ == "__main__":
